f4344650bc987fbc0f341a259aa1316e1b109fab
datCodeGirl <adeolamarsh@gmail.com>
2024-07-08 22:09:28 -0400

    git commit -m "created a fetch transacation"

--- src/Components/Transactions.jsx
+++ src/Components/Transactions.jsx
@@ -1,8 +1,29 @@
 import React from "react";
+import { useState, useEffect } from "react";
+
+const API = import.meta.env.VITE_API_URL
 
 function Transactions() {
-     return (
-        <div>Transactions</div>
+   const [transactions,setTransactions] =useState([])
+      useEffect(() => {
+            fetch(`${API}/transactions`)
+            .then((res)=> {
+            return res.json() 
+            })
+            .then(resJSON => {
+               console.log(resJSON)
+               setTransactions(resJSON)
+            })
+          .catch((error)=> console.error(error))
+         }, [])
+      
+   return (
+          <div>
+            <h1>Transactions</h1>
+            {transactions.map(transaction => {
+               return <p key={transaction.id}>{transaction.type}</p>
+   })}
+        </div>
      )
 }
 
